7c88c3c214cbf7ebba1cc452a372ad1fe1dc6d96
natthawanbdk <natthawan.bdk@gmail.com>
2026-04-07 11:43:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ========== RESUME DATA ==========
 const resumeData = {
     personalInfo: {
-        photo: 'images/photo.jpg',
+        photo: 'https://cdn.phototourl.com/free/2026-04-07-fcf60d49-f112-46b7-b796-94704bbf139e.jpg',
         name: 'Natthawan Duangkaew',
         phone: '+66 87 160 5848',
         email: 'natthawan.bdk@gmail.com',
